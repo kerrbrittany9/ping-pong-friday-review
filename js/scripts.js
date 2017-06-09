db0bbd2109b3e631922f41e6a6988428d47d485c
Brittany Kerr <kerrbrittany9@gmail.com>
2017-06-09 15:23:25 -0700

    add var countBy 1 to for loop as initialization parameter

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 var listedNumbers = [];
+var countBy = [1];
 function counting(countTo) {
   var result = "";
-  for (var index = 0; index <= countTo; index += 1);
-
+  for (var index = countBy; index <= countTo; index += countBy); {
+  alert(getting number)
+  return listedNumbers.push(index + ",");
+  }
   if ((index % 3 === 0) && (index % 5 !== 0)) {
   result = "ping";
   } else if ((index % 5 === 0) && (index % 3 !== 0)){
@@ -10,15 +13,13 @@ function counting(countTo) {
   } else if ((index % 3 === 0) && (index % 5 === 0)) {
   result = "ping-pong";
   }
-  return listedNumbers.push(index + ",");
-  }
 $(document).ready(function() {
   $("form#numberEntered").submit(function(event) {
     event.preventDefault();
     var countTo = parseInt($("input#count-to").val());
       $("#result").show(counting(countTo));
-      $("#result").click(function(){
-      $(this).remove();
+      $("#result").click(function() {
+      $(this).hide();
     });
   });
 });
