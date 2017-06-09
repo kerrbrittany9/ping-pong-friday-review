fa9e74fb9944eb642fcf54d3563e17d1aef61278
Brittany Kerr <kerrbrittany9@gmail.com>
2017-06-09 13:26:17 -0700

    change js .click function to correct ID- form#numberEntered

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,27 @@
-function counting(countTo) {
-  var countBy = [1];
-  var listedNumbers = [];
-  var result;
-  for (var index = countBy; index <= countTo; index += countBy); { return listedNumbers.push(index + ",");
-  }
-    if ((index % 3 === 0) && (index % 5 !== 0)) {
-    result = "ping";
-  } else if ((index % 5 === 0) && (index % 3 !== 0)){
-    result = "pong";
-  } else if ((index % 3 === 0) && (index % 5 === 0)) {
-    result = "ping-pong";
-  }
-      console.log(result);
-      return result;
+$(document).ready(function() {
+  $("form#numberEntered").click(function(event) {
+    event.preventDefault();
+    var countTo = parseInt($("input#count-to").val());
 
-    }
-  $(document).ready(function() {
-    $("submitButton").click(function(event) {
-      event.preventDefault();
-      var countTo = parseInt($("input#count-to").val());
+    function counting(countTo) {
+      var countBy = [1];
+      var listedNumbers = [];
+      var result;
+      for (var index = countBy; index <= countTo; index += countBy); { return listedNumbers.push(index + ",");
+      }
+      debugger
+        if ((index % 3 === 0) && (index % 5 !== 0)) {
+        result = "ping";
+      } else if ((index % 5 === 0) && (index % 3 !== 0)){
+        result = "pong";
+      } else if ((index % 3 === 0) && (index % 5 === 0)) {
+        result = "ping-pong";
+      }
+        console.log(result);
+        return result;
+
+      }
+
       $("#result").show(countTo);
       $("#result").click(function(){
       $(this).remove();
