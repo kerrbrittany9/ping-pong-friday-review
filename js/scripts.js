3fcaa7853fb26b3f432338ff8bcf71f8f857a392
Brittany Kerr <kerrbrittany9@gmail.com>
2017-06-09 09:50:06 -0700

    add function for single digit number that branches for ping and pong result

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
 
-// var countedArray = [];
-function counting(number) {
-  for (var index = 0; index <= number; index += 1)  {
-      countedArray.push(index + ",");
-      alert(newNumber)
+// get input number to return in a new array, count
 
-  });
-});
+function singleDigit(number) {
+  var result = "";
+  if (number % 3 === 0) {
+      result = "ping";
+  } else if (number % 5 === 0) {
+    result = "pong";
+  };
+  console.log(result);
+  return result;
+};
+
+
 $(document).ready(function() {
   $("submitButton").click(function(event) {
     event.preventDefault();
