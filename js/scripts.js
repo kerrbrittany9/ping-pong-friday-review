76362690d0fcca05b126da4a4489ffb4133c712d
Brittany Kerr <kerrbrittany9@gmail.com>
2017-06-09 11:22:27 -0700

    add click function to remove result

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-
+// back-end logic//
 var counting = function(countTo)
 var countBy = [1];
 var listedNumbers = [];
@@ -11,12 +11,14 @@ for (var index = countBy; index <= countTo; index += countBy); { listedNumbers.p
     result = "pong";
   } else if ((index % 3 === 0) && (index % 5 === 0)) {
 };
-$(document).ready(function() {
-  $("submitButton").click(function(event) {
-    event.preventDefault();
-    var countTo = parseInt($("input#count-to").val());
-    var result = counting(countTo);
-    $("#result").append(result);
-    $(this).remove();
+    $(document).ready(function() {
+    $("submitButton").click(function(event) {
+      event.preventDefault();
+      var countTo = parseInt($("input#count-to").val());
+      var result = counting(countTo);
+      $("#result").show(result);
+      $("#result").click(function(){
+      $(this).remove();
+    });
   });
 });
