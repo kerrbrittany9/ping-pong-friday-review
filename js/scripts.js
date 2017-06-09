0b61ff5c03f36a8cef6d65c02877c700abaf2f45
Brittany Kerr <kerrbrittany9@gmail.com>
2017-06-09 10:20:55 -0700

    add if / else statements for results that are divisible by 3, 5, or both

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,22 @@
 
-// get input number to return in a new array, count
-var countBy = [1]
 
-for (var index = countBy; index <= countTo; index += countBy)
+var countTo = parseInt($("input#countTo").val());
+var countBy = [1];
+var listedNumbers = [];
+for (var index = countBy; index <= countTo; index += countBy); { listedNumbers.push(index + ",");
+}
 
-
-function singleDigit(number) {
-  var result = "";
-  if (number % 3 === 0) {
+  if ((index % 3 === 0) && (index % 5 !== 0)) {
       result = "ping";
-  } else if (number % 5 === 0) {
+  } else if ((index % 5 === 0) && (index % 3 !== 0)){
     result = "pong";
-  };
-  console.log(result);
-  return result;
+  } else if ((index % 3 === 0) && (index % 5 === 0))
 };
-
-function doubleDigit()
 $(document).ready(function() {
   $("submitButton").click(function(event) {
     event.preventDefault();
-    var countTo = parseInt($("input#countTo").val());
-    $("#result").append(number);
+
+    $("#result").append(listedNumbers);
     $("#result").show();
   });
 });
