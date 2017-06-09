d5413c0faf0d621aa93f848c4a4d65c2160af698
Brittany Kerr <kerrbrittany9@gmail.com>
2017-06-09 14:20:55 -0700

    change .click to .submit in submit function

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,25 @@
+    //
 
-    var countTo = parseInt($("input#count-to").val());
-    var countBy = [1];
-    var listedNumbers = [];
-    function counting(countTo) {
-      var result = "";
-      for (var index = countBy; index <= countTo; index += countBy); { return listedNumbers.push(index + ",");
-      }
-      debugger
-        if ((index % 3 === 0) && (index % 5 !== 0)) {
-        result = "ping";
-      } else if ((index % 5 === 0) && (index % 3 !== 0)){
-        result = "pong";
-      } else if ((index % 3 === 0) && (index % 5 === 0)) {
-        result = "ping-pong";
-      }
-        return result;
-      }
+    // var countBy = [1];
+    // var listedNumbers = [];
+    // function counting(countTo) {
+    //   var result = "";
+    //   for (var index = countBy; index <= countTo; index += countBy); { return listedNumbers.push(index + ",");
+    //   }
+    //   debugger
+    //     if ((index % 3 === 0) && (index % 5 !== 0)) {
+    //     result = "ping";
+    //   } else if ((index % 5 === 0) && (index % 3 !== 0)){
+    //     result = "pong";
+    //   } else if ((index % 3 === 0) && (index % 5 === 0)) {
+    //     result = "ping-pong";
+    //   }
+    //     return result;
+    //   }
   $(document).ready(function() {
-    $("form#numberEntered").click(function(event) {
+    $("form#numberEntered").submit(function(event) {
       event.preventDefault();
+      var countTo = parseInt($("input#count-to").val());
       $("#result").append(countTo);
       $("#result").click(function(){
       $(this).remove();
