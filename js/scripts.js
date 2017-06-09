1c02f7dc98ed38ebeb4d1a78839d95cdd281f0cb
Brittany Kerr <kerrbrittany9@gmail.com>
2017-06-09 08:38:56 -0700

    add js submit function

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// var  = parseInt($("input#number").val());
+//
+//     for (var index = countByNumber; index < countToNumber; index += countByNumber) {
+//         countedArray.push(index + ",");
+//     }
+$(document).ready(function() {
+  $("submitButton").click(function(event) {
+    event.preventDefault();
+  });
+});
