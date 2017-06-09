2a7c2debd86fb5236b8ff59fa4a413c55f23f3d1
Brittany Kerr <kerrbrittany9@gmail.com>
2017-06-09 08:42:45 -0700

    make input into integer and split it

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
-// var  = parseInt($("input#number").val());
-//
-//     for (var index = countByNumber; index < countToNumber; index += countByNumber) {
-//         countedArray.push(index + ",");
-//     }
+var newNumber = parseInt($("input#number").val()).split(" ");
+
+    }
 $(document).ready(function() {
   $("submitButton").click(function(event) {
     event.preventDefault();
