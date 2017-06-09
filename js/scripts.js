2289ed33d7bec25dd1e5da9099a846249039ac72
Brittany Kerr <kerrbrittany9@gmail.com>
2017-06-09 16:21:48 -0700

    add image in well nested with result

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
 var listedNumbers = [];
 var countBy = [1];
-function counting(countTo) {
-  var result = "";
-  for (var index = countBy; index <= countTo; index += countBy); {
-  return listedNumbers.push(index + ",");
+// function counting(countTo) {
+//   var result = "";
+for (var index = countBy; index <= countTo; index += countBy); {
+  var listedNumbers = listedNumbers.push(index + ",");
+  return listedNumbers
   }
   if ((index % 3 === 0) && (index % 5 !== 0)) {
-  result = "ping";
+    result = "ping";
   } else if ((index % 5 === 0) && (index % 3 !== 0)){
-  result = "pong";
+    result = "pong";
   } else if ((index % 3 === 0) && (index % 5 === 0)) {
-  result = "ping-pong";
+    result = "ping-pong";
   }
 };
 $(document).ready(function() {
