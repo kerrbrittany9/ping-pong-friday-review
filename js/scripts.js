7a5cef85eab1d5c37ab0ae9fce5d1984a65e2611
Brittany Kerr <kerrbrittany9@gmail.com>
2017-06-09 09:18:18 -0700

    lower var number input down to UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
-var newNumber = parseInt($("input#number").val());
-var countedArray = [];
-  for (var index = 0; index < newNumber; index += 1)  {
+
+// var countedArray = [];
+function counting(number) {
+  for (var index = 0; index <= number; index += 1)  {
       countedArray.push(index + ",");
-  }
-    }
+      alert(newNumber)
+
+  });
+});
 $(document).ready(function() {
   $("submitButton").click(function(event) {
     event.preventDefault();
-    $("#result").append();
+    var number = parseInt($("input#number").val());
+    $("#result").append(number);
+    $("#result").show();
   });
 });
