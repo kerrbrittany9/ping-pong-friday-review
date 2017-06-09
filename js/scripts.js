01860c1b389ec1c2f3fdbee6fd78aca0eee16123
Brittany Kerr <kerrbrittany9@gmail.com>
2017-06-09 08:51:06 -0700

    remove split method from input

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
-var newNumber = parseInt($("input#number").val()).split(" ");
-
+var newNumber = parseInt($("input#number").val());
+var countedArray = [];
+  for (var index = 0; index < newNumber; index += 1)  {
+      countedArray.push(index + ",");
+  }
     }
 $(document).ready(function() {
   $("submitButton").click(function(event) {
     event.preventDefault();
+    $("#result").append();
   });
 });
