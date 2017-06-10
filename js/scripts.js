31b1cfbe5b50e82a40c22a010a9e656ea978ecbe
Brittany Kerr <kerrbrittany9@gmail.com>
2017-06-10 11:30:27 -0700

    reverse and simplify the if/else if statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,21 @@
+
+function counting(countTo) {
 var listedNumbers = [];
 var countBy = [1];
-// function counting(countTo) {
+function counting(countTo) {
 //   var result = "";
 for (var index = countBy; index <= countTo; index += countBy); {
   var listedNumbers = listedNumbers.push(index + ",");
   return listedNumbers
   }
-  if ((index % 3 === 0) && (index % 5 !== 0)) {
+  if (index % 15 === 0) {
     result = "ping";
-  } else if ((index % 5 === 0) && (index % 3 !== 0)){
+  } else if (index % 5 === 0) {
     result = "pong";
-  } else if ((index % 3 === 0) && (index % 5 === 0)) {
+  } else if (index % 3 === 0) {
     result = "ping-pong";
+  } else {
+    listedNumbers.push(index)
   }
 };
 $(document).ready(function(){
