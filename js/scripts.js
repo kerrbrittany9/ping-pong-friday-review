9c77c7f0d4f4c1626439bf1d1578c33f5e743423
Brittany Kerr <kerrbrittany9@gmail.com>
2017-06-10 11:51:25 -0700

    declare and define result variable

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,24 @@ var counting = function(countTo){
 var listedNumbers = [];
 var countBy = [1];
 for (var index = countBy; index <= countTo; index += countBy)
-  {  
-  if (index % 15 === 0) {
-    listedNumbers.push("ping-pong")
-  } else if (index % 5 === 0) {
-    listedNumbers.push("pong")
-  } else if (index % 3 === 0) {
-    listedNumbers.push("ping")
-  } else {
-    listedNumbers.push(index)
-  }
+  {
+    if (index % 15 === 0) {
+      listedNumbers.push("ping-pong")
+    } else if (index % 5 === 0) {
+      listedNumbers.push("pong")
+    } else if (index % 3 === 0) {
+      listedNumbers.push("ping")
+    } else {
+      listedNumbers.push(index)
+    }
+  } return listedNumbers;
 };
 $(document).ready(function(){
   $("form#numberEntered").submit(function(event){
     event.preventDefault();
     var countTo = parseInt($("input#countTo").val());
+
+    var result = counting(countTo);
       $("#result").show(listedNumbers);
       $("#result").click(function() {
       $(this).hide();
