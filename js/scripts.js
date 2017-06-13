08b5a78bc0ee53e949f0dee0245a435be6057b28
Brittany Kerr <kerrbrittany9@gmail.com>
2017-06-12 21:17:30 -0700

    change input ID to hyphenated, change index to i

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,27 @@
 // business
 var counting = function(countTo){
-var listedNumbers = [];
-var countBy = [1];
-for (var index = countBy; index <= countTo; index += countBy)
-  {
-    if (index % 15 === 0) {
+  var listedNumbers = [];
+
+  for (var i = countBy; i <= countTo; i += countBy)
+
+    if (i % 15 === 0) {
       listedNumbers.push("ping-pong")
-    } else if (index % 5 === 0) {
+    } else if (i % 5 === 0) {
       listedNumbers.push("pong")
-    } else if (index % 3 === 0) {
+    } else if (i % 3 === 0) {
       listedNumbers.push("ping")
     } else {
-      listedNumbers.push(index)
+      listedNumbers.push(i)
     }
-  } return listedNumbers;
+  // } return listedNumbers;
 };
 
 // user interface
 $(document).ready(function(){
-  $("form#numberEntered").submit(function(event){
+  $("form#number-entered").submit(function(event){
+    alert(hey);
     event.preventDefault();
-    var countTo = parseInt($("input#countTo").val());
+    var countTo = parseInt($("input#count-to").val());
     $("#result").empty();
      var result = counting(countTo);
      $("#result").append("<li>" + result + "</li>");
